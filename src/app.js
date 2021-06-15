4301f0c43c7510fb29b2d3be48df3a65ab87a56f
Parth Dhawan <pdhawan2001@gmail.com>
2021-06-15 22:52:20 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const request = require('request')
 const address = process.argv[2]
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -87,6 +88,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
